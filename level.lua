fe044b705555a63717666c46f9a8882cb86c0f22
Ross Andrews <randrews@geekfu.org>
2012-08-04 01:03:24 -0500

    Some physics tweaking, crates, moved a lot of code to level.lua.

--- level.lua
+++ level.lua
@@ -13,16 +13,19 @@ function new(strs)
 end
 
 function methods:init()
+   self.world = love.physics.newWorld(0, 0)
+   self:makeEdges()
+   self:makeWalls()
+   self:makeCrates(world)
+   self.player = self:makePlayer()
 end
 
-function methods:makeEdges(world)
-   assert(world)
-
+function methods:makeEdges()
    local w = SIZE * self.width
    local h = SIZE * self.height
 
    local edge = {}
-   edge.body = love.physics.newBody(world, 0, 0, 'static')
+   edge.body = love.physics.newBody(self.world, 0, 0, 'static')
    edge.shapes = {}
    table.insert(edge.shapes, love.physics.newEdgeShape(0, 0, w, 0))
    table.insert(edge.shapes, love.physics.newEdgeShape(0, 0, 0, h))
@@ -32,9 +35,31 @@ function methods:makeEdges(world)
    return edge
 end
 
-function methods:makeWalls(world)
+function methods:makeCrates()
    local ph = love.physics
-   local walls = ph.newBody(world, 0, 0, 'static')
+   self.crates = {}
+
+   for p in self:each() do
+      if self(p) == 'o' then
+         local b = ph.newBody(self.world,
+                              p.x*SIZE + SIZE/2,
+                              p.y*SIZE + SIZE/2,
+                              'dynamic')
+         local s = ph.newRectangleShape(0, 0,
+                                        SIZE, SIZE)
+
+         b:setMass(0.02)
+         b:setLinearDamping(1)
+         b:setFixedRotation(true)
+         ph.newFixture(b, s)
+         table.insert(self.crates, {body=b, shape=s})
+      end
+   end         
+end
+
+function methods:makeWalls()
+   local ph = love.physics
+   local walls = ph.newBody(self.world, 0, 0, 'static')
 
    for p in self:each() do
       if self(p) == '#' then
@@ -45,14 +70,116 @@ function methods:makeWalls(world)
    end
 end
 
-function methods:makePlayer(world)
+function methods:makePlayer()
    local player = {}
    local ph = love.physics
 
-   player.body = ph.newBody(world, SIZE*1.5, SIZE*1.5, 'dynamic')
+   player.body = ph.newBody(self.world, SIZE*1.5, SIZE*1.5, 'dynamic')
    player.shape = ph.newCircleShape(10)
    ph.newFixture(player.body, player.shape)
 
    player.body:setMass(0.01)
    return player
+end
+
+function methods:draw()
+   local g = love.graphics
+
+   -- Draw ground
+   g.setColor(64, 120, 64)
+   g.rectangle('fill', 0, 0, self.width*SIZE, self.height*SIZE)
+
+   -- if player.location then
+   --    g.setColor(255,255,255)
+   --    local l = player.location * SIZE
+   --    g.rectangle('line', l.x, l.y, SIZE, SIZE)
+
+   --    if player.direction then
+   --       g.setColor(255,0,0)
+   --       local t = (player.location + player.direction) * SIZE
+   --       g.rectangle('line', t.x, t.y, SIZE, SIZE)
+   --    end
+   -- end
+
+   -- Draw player
+   g.setColor(160, 64, 64)
+   g.circle('fill',
+            self.player.body:getX(), self.player.body:getY(), SIZE/2)
+
+   -- Draw walls
+   g.setColor(155, 155, 155)
+   for p in self:each() do
+      if self(p) == '#' then
+         g.rectangle('fill', p.x*SIZE, p.y*SIZE, SIZE, SIZE)
+      end
+   end
+
+   g.setColor(180, 120, 90)
+   for _, c in ipairs(self.crates) do
+      g.rectangle('fill', c.body:getX()-SIZE/2, c.body:getY()-SIZE/2, SIZE, SIZE)
+   end
+end
+
+function methods:update(dt)
+   local f = 300 * dt
+   local k = love.keyboard.isDown
+   local kd = 0
+   local dir = nil
+
+   local p = self.player
+
+   if k('up') then p.body:applyForce(0, -f) ; dir = point.up ; kd = kd + 1 end
+   if k('down') then p.body:applyForce(0, f) ; dir = point.down ; kd = kd + 1 end
+   if k('left') then p.body:applyForce(-f, 0) ; dir = point.left ; kd = kd + 1 end
+   if k('right') then p.body:applyForce(f, 0) ; dir = point.right ; kd = kd + 1 end
+
+   if kd > 0 then
+      p.body:setLinearDamping(0.25)
+   else
+      p.body:setLinearDamping(15)
+   end
+
+   p.location = point(
+      math.floor(p.body:getX() / SIZE),
+      math.floor(p.body:getY() / SIZE))
+
+   if kd == 1 then
+      p.direction = dir
+      self:nudgeToSquare(p.body, p.location + p.direction, dt)
+   elseif kd == 0 then
+      self:nudgeToSquare(p.body, p.location, dt)
+   end
+
+   for _, c in ipairs(self.crates) do
+      local sq = point(
+         math.floor(c.body:getX() / SIZE),
+         math.floor(c.body:getY() / SIZE))
+      self:nudgeToSquare(c.body, sq, dt)
+   end
+
+   self:max_speed(p.body, 300)
+   self.world:update(dt)
+end
+
+function methods:nudgeToSquare(body, sq, dt)
+   local acc = 25
+
+   local y = body:getY() - SIZE/2
+   local ty = sq.y * SIZE
+   local f = dt * acc * (ty - y)
+   body:applyForce(0, f)
+
+   local x = body:getX() - SIZE/2
+   local tx = sq.x * SIZE
+   local f = dt * acc * (tx - x)
+   body:applyForce(f, 0)
+end
+
+function methods:max_speed(body, spd)
+   local x, y = body:getLinearVelocity()
+   if x*x + y*y > spd*spd then
+      local a = math.atan2(y,x)
+      body:setLinearVelocity(spd * math.cos(a),
+                             spd * math.sin(a))
+   end
 end
--- main.lua
+++ main.lua
@@ -3,17 +3,14 @@ assert(love, "Run this inside Love")
 local Level = require('level')
 local point = require('point')
 
-local world = nil
-
-local edge = nil
-local player = nil
+love.physics.setMeter(32)
 
 local level = Level.new{
    "..........######.........",
    "..........#..............",
    "..........#.#............",
    ".....######.#............",
-   "............#............",
+   "............#......o.....",
    ".....##..##.#............",
    "......#..##.#............",
    "......#..#........#......",
@@ -29,98 +26,12 @@ local level = Level.new{
 
 function love.load()
    math.randomseed(os.time())
-
-   love.physics.setMeter(32)
-   world = love.physics.newWorld(0, 0)
-
-   edge = level:makeEdges(world)
-   level:makeWalls(world)
-   player = level:makePlayer(world)
 end
 
 function love.draw()
-   local g = love.graphics
-
-   g.setColor(64, 120, 64)
-   g.rectangle('fill', 0, 0, level.width*32, level.height*32)
-
-   if player.location then
-      g.setColor(255,255,255)
-      local l = player.location * 32
-      g.rectangle('line', l.x, l.y, 32, 32)
-
-      if player.direction then
-         g.setColor(255,0,0)
-         local t = (player.location + player.direction) * 32
-         g.rectangle('line', t.x, t.y, 32, 32)
-      end
-   end
-
-   g.setColor(160, 64, 64)
-   g.circle('fill',
-            player.body:getX(), player.body:getY(), 16)
-
-   g.setColor(155, 155, 155)
-   for p in level:each() do
-      if level(p) == '#' then
-         g.rectangle('fill', p.x*32, p.y*32, 32, 32)
-      end
-   end
-end
-
-function love.mousepressed(mouse_x, mouse_y)
-end
-
-function love.mousereleased()
+   level:draw()
 end
 
 function love.update(dt)
-   local f = 1500 * dt
-   local k = love.keyboard.isDown
-   local kd = 0
-   local dir = nil
-
-   if k('up') then player.body:applyForce(0, -f) ; dir = point.up ; kd = kd + 1 end
-   if k('down') then player.body:applyForce(0, f) ; dir = point.down ; kd = kd + 1 end
-   if k('left') then player.body:applyForce(-f, 0) ; dir = point.left ; kd = kd + 1 end
-   if k('right') then player.body:applyForce(f, 0) ; dir = point.right ; kd = kd + 1 end
-
-   if kd > 0 then
-      player.body:setLinearDamping(0.25)
-   else
-      player.body:setLinearDamping(15)
-   end
-
-   player.location = point(
-      math.floor(player.body:getX() / 32),
-      math.floor(player.body:getY() / 32))
-
-   if kd == 1 then
-      player.direction = dir
-      local acc = 25
-
-      if player.direction.y == 0 then -- horizontal
-         local y = player.body:getY()-16
-         local ty = player.location.y * 32
-         local f = dt * acc * (ty - y)
-         player.body:applyForce(0, f)
-      else -- vertical
-         local x = player.body:getX()-16
-         local tx = player.location.x * 32
-         local f = dt * acc * (tx - x)
-         player.body:applyForce(f, 0)
-      end
-   end
-
-   max_speed(player.body, 300)
-   world:update(dt)
-end
-
-function max_speed(body, spd)
-   local x, y = body:getLinearVelocity()
-   if x*x + y*y > spd*spd then
-      local a = math.atan2(y,x)
-      body:setLinearVelocity(spd * math.cos(a),
-                             spd * math.sin(a))
-   end
+   level:update(dt)
 end
